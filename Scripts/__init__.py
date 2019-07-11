214d8c4504214c5c42e506ca9b01618607d49998
aviban99 <bansal.avishi11@gmail.com>
2019-07-11 14:05:52 +0530

    Update __init__.py

--- Scripts/__init__.py
+++ Scripts/__init__.py
@@ -1,1 +1,3 @@
-
+# dataset is copyrighted by the Radboud University
+# it is suggested you download it from their website given in README.md 
+# some libraries are imported may not be used be in the codes
